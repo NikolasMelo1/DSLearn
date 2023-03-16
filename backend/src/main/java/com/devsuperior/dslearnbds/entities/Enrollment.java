4db4596549dcc520a595ca83d8083e26b776f015
NikolasMelo1 <nikolas_paulo@hotmail.com>
2023-03-16 14:09:13 -0300

    :construction: Create Enrollment and EnrollmentPK

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.time.Instant;
+
+public class Enrollment {
+
+
+    private Instant enrollMoment;
+    private Instant refundMoment;
+    private boolean available;
+    private boolean onlyUpdate;
+}
